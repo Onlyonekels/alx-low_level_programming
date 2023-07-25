6e8b0c227813cb19f0e35105c0a918df3f5664e8
Onlyonekels <onorpatrick@gmail.com>
2023-07-25 03:31:07 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,11 +8,11 @@ int _strlen(char *s)
 {
 int longi = 0;
 
-         while (*s l= '\0')
-         {
+while (*s l = '\0')
+	{
                  longi++;
                  s++
-         }
+	}
 
 	return (longi);
 }
