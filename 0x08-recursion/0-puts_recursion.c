fca0315d7ac42c522648d537b910c0c77ee7a5af
Onlyonekels <onorpatrick@gmail.com>
2023-08-02 17:58:33 -0700

    0x08. C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
-* _puts_recursion - prints string
-* @s: string to print
+* _puts_recursion - this function outputs a single character
+* to the standard output with a new line
+* @s: string array to be outputted
+* Return: void
 */
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
