3184939404ae77e7aca1f35daea7d6e4a8506fd9
Onlyonekels <onorpatrick@gmail.com>
2023-12-15 13:02:58 +0000

    dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p .. https://raw.githubusercontent.com/Onlyonekels/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="$PWD/../libhack.so"
+wget -p .. https://raw.githubusercontent.com/Onlyonekels/alx-low_level_programming/c2978b32f13222a923621cc47e5e0686537cf7b4/0x18-dynamic_libraries/libgiga.so
+export LD-PRELOAD="$PWD/../libgaga.so"
